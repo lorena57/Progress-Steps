d77f8d1102a1ac6abe335a6c27b7f4adbb9eee30
Lorena C <lorena@eunknown.org>
2022-02-01 20:33:30 -0800

    recopy in css, add if functions

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const progress = document.getElementById('progress');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
-const circles = document.querySelectorAll('circles');
+const circles = document.querySelectorAll('.circle');
 
 let currentActive = 1;
 
@@ -12,6 +12,7 @@ next.addEventListener('click', () => {
   if (currentActive > circles.length) {
     currentActive = circles.length;
   }
+  update();
 });
 
 prev.addEventListener('click', () => {
@@ -19,4 +20,28 @@ prev.addEventListener('click', () => {
   if (currentActive < 1) {
     currentActive = 1;
   }
+  update();
 });
+
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
+
+  const actives = document.querySelectorAll('.active');
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
+
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
